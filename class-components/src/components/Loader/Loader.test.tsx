b281951e4050d2ee35d9237958569274bcc8d72d
scheslen <antgalin@mail.ru>
2025-07-20 17:51:18 +0300

    feat: create example test, Header test, Loader test

--- class-components/src/components/Loader/Loader.test.tsx
+++ class-components/src/components/Loader/Loader.test.tsx
@@ -1,6 +1,13 @@
-import { describe, test, expect } from "vitest";
-describe("Loader", () => {
-  test("LLLL", () => {
-    expect(1 + 1).toBe(2);
+
+import { render } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { Loader } from './Loader';
+
+describe('Loader Component', () => {
+  it('renders class "loader"', () => {
+    const { container } = render(<Loader />);
+    const loaderDiv = container.querySelector('.loader');
+    expect(loaderDiv).toBeInTheDocument();
   });
 });
+
