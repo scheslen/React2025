e232905bc659820332b5afc7d81f38e613ab0c32
scheslen <antgalin@mail.ru>
2025-07-20 22:28:00 +0300

    refactor: format files

--- class-components/src/components/Loader/Loader.test.tsx
+++ class-components/src/components/Loader/Loader.test.tsx
@@ -1,13 +1,11 @@
+import { render } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { Loader } from "./Loader";
 
-import { render } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
-import { Loader } from './Loader';
-
-describe('Loader Component', () => {
+describe("Loader Component", () => {
   it('renders class "loader"', () => {
     const { container } = render(<Loader />);
-    const loaderDiv = container.querySelector('.loader');
+    const loaderDiv = container.querySelector(".loader");
     expect(loaderDiv).toBeInTheDocument();
   });
 });
-
