911353e7980f51d99168a4a0cf0284c7e1859240
scheslen <antgalin@mail.ru>
2025-08-21 21:54:32 +0300

    feat: add ErrorBoundary component

--- forms-app/src/index.tsx
+++ forms-app/src/index.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ErrorBoundary } from "./components/ErrorBoundary/ErrorBoundary.tsx";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+       <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
